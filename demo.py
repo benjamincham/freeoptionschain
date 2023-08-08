a0e33f51ac9c0e7aff7d3cb6b19445eed87c8614
benjamin <benjamin_cham@htx.gov.sg>
2023-08-09 00:59:12 +0800

    fix: replace db.py with db.cpython-38-x86_64-linux-gnu.so

--- demo.py
+++ demo.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
 
     ref_FOC = FOC()
     
-    # options_price_data = ref_FOC.get_options_price_data(ref_FOC.get_contract_symbol("AMC",'2023-08-11','CALL',4.0))
-    # print(options_price_data)
+    options_price_data = ref_FOC.get_options_price_data(ref_FOC.get_contract_symbol("AMC",'2023-08-11','CALL',4.0))
+    print(options_price_data)
     
     stocks_price_data = ref_FOC.get_stock_price("AAPL")
     print(stocks_price_data)
